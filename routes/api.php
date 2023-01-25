70f8558204613ebfdd51fe123added95ea5b076f
IvonneSDQ <73309426+IvonneSDQ@users.noreply.github.com>
2023-01-24 21:04:38 -0500

    se sube el proyecto

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\Api\PortafolioController;
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::resource('portafolios', PortafolioController::class);
+
+// Ruta pública para el manejo de inicio de sesión del usuario
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Grupo de rutas protegidas
+Route::middleware(['auth:sanctum'])->group(function ()
+{
+    // Ruta para el cierre de sesión
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
+
